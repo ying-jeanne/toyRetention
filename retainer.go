31feceba98b615848302c0e496812415e9863ed4
Tyler Reid <tyler.reid@grafana.com>
2023-06-13 14:33:51 -0500

    Reformat the files and add some more tests

--- retainer.go
+++ retainer.go
@@ -27,32 +27,33 @@ type userConfig struct {
 	policies      []perSeriesRetentionPolicy
 }
 
-// Returns true if the maxTime is after the threshold and would be expired
+func ApplyBucketRetention(policies userConfig, userBucket *bucket, currentTime int64) {
+	for i, b := range userBucket.blocks {
+		// If the block is outside the default retention tier it will need to be retained in some way
+		if !checkBlockRetention(b.maxT, currentTime-policies.baseRetention) {
+			return
+		} else {
+			// Apply all valid policies to the block and update the block
+			userBucket.blocks[i] = applyPolicy(buildPolicy(b.maxT, policies, currentTime), b)
+		}
+	}
+}
+
+// Returns true if the maxTime is outside the retention threshold and would be expired
 func checkBlockRetention(maxT int64, threshold int64) bool {
-	// if max t is before at threshold time
+	// if max t is before threshold time return true
 	if maxT <= threshold {
 		return true
 	}
 	return false
 }
 
-func ApplyBucketRetention(policies userConfig, userBucket *bucket, currentTime int64) {
-	for i, b := range userBucket.blocks {
-		if !checkBlockRetention(b.maxT, currentTime-policies.baseRetention) {
-			return
-		} else {
-			userBucket.blocks[i] = applyPolicy(buildPolicy(b.maxT, policies, currentTime), b)
-		}
-	}
-
-}
-
 func buildPolicy(blockMaxTime int64, config userConfig, currentTime int64) []perSeriesRetentionPolicy {
 	// Figure out which policy matches
-	// If the time is explicitly after the threshold apply that config
+	// If the time is explicitly after the threshold apply any config that is still valid
 	var toApply []perSeriesRetentionPolicy
 	for _, p := range config.policies {
-		// You want the inverse, so any poilcy that is still valid we would want to apply
+		// You want the inverse, so any policy that is still valid we would want to apply
 		if !checkBlockRetention(blockMaxTime, currentTime-p.retentionPeriod) {
 			toApply = append(toApply, p)
 		}
--- retainer_test.go
+++ retainer_test.go
@@ -51,3 +51,16 @@ func TestOutsideDefaultRetentionOverlap(t *testing.T) {
 	ApplyBucketRetention(p, &bucket, 3)
 	assert.Equal(t, 2, len(bucket.blocks[0].series))
 }
+
+func TestRetentionTierMoveTime(t *testing.T) {
+	b := block{minT: 0, maxT: 1, series: map[string]interface{}{"series1": struct{}{}, "series2": struct{}{}, "series3": struct{}{}}}
+	bucket := bucket{blocks: []block{b}}
+	// Make the blocks into a map probably
+	p := userConfig{baseRetention: 2, policies: []perSeriesRetentionPolicy{{retentionPeriod: 3, policy: "series1"}, {retentionPeriod: 5, policy: "series2"}}}
+	ApplyBucketRetention(p, &bucket, 3)
+	p2 := userConfig{baseRetention: 2, policies: []perSeriesRetentionPolicy{{retentionPeriod: 4, policy: "series1"}, {retentionPeriod: 5, policy: "series2"}}}
+	ApplyBucketRetention(p2, &bucket, 4)
+	assert.Equal(t, 2, len(bucket.blocks[0].series))
+	ApplyBucketRetention(p2, &bucket, 5)
+	assert.Equal(t, 1, len(bucket.blocks[0].series))
+}
